79ca3b53adb8fe7a6657d4ec486679a8ae092d97
Zaeem Altaf <official.zaeemaltaf@gmail.com>
2025-01-29 00:04:57 +0500

    refference issue

--- src/sanity/reviews/getProductReviews.ts
+++ src/sanity/reviews/getProductReviews.ts
@@ -6,7 +6,7 @@ export const getProductReviews = async (productId: string): Promise<Review[]> =>
   console.log(productId)
   try {
     const PRODUCT_REVIEW_BY_ID = defineQuery(`
-      *[_type == "review" && references(^._id) && references(^._id)] `)
+      *[_type == "review" && references($productId)] `);
     const params = { productId };
     const reviews = await client.fetch(PRODUCT_REVIEW_BY_ID, params);
     return reviews || [];
